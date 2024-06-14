ef95751a11e8b536472f12d2d0710f6b3d63970f
migodlcrz <lorenzo.cap.delacruz@gmail.com>
2024-06-14 23:26:44 +0800

    added endpoint routes for
    getrefs
    getlist
    getid

--- packages/functions/src/routes.ts
+++ packages/functions/src/routes.ts
@@ -1,22 +1,40 @@
-import { envVariables } from "./variables"
+import { envVariables } from "./variables";
 
 export const routes = {
-    "GET /api/getProj": {
-      function: {
-        handler: "packages/functions/src/testRail.getProj",
-        environment: envVariables
-      },
+  "GET /api/getProj": {
+    function: {
+      handler: "packages/functions/src/testRail.getProj",
+      environment: envVariables,
     },
-    "POST /api/editCustomFields": {
-      function: {
-        handler: "packages/functions/src/testRail.editCustomFields",
-        environment: envVariables
-      },
+  },
+  "POST /api/editCustomFields": {
+    function: {
+      handler: "packages/functions/src/testRail.editCustomFields",
+      environment: envVariables,
     },
-    "GET /api/updateCase": {
-      function: {
-        handler: "packages/functions/src/testRail.updateCase",
-        environment: envVariables
-      },
+  },
+  "POST /api/updateCase": {
+    function: {
+      handler: "packages/functions/src/testRail.updateCase",
+      environment: envVariables,
     },
-  }
+  },
+  "POST /api/getRefs": {
+    function: {
+      handler: "packages/functions/src/testRail.getRefs",
+      environment: envVariables,
+    },
+  },
+  "POST /api/getList": {
+    function: {
+      handler: "packages/functions/src/testRail.getList",
+      environment: envVariables,
+    },
+  },
+  "POST /api/getID": {
+    function: {
+      handler: "packages/functions/src/testRail.getID",
+      environment: envVariables,
+    },
+  },
+};
